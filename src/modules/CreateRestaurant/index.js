b7a810444749b14dab7b16655eb28af1c4ab3740
Marlon Fleurencois <marlonfleurencois@gmai.com>
2023-01-31 10:38:26 -0500

    January 31

--- src/modules/CreateRestaurant/index.js
+++ src/modules/CreateRestaurant/index.js
@@ -1,7 +1,47 @@
+import { Card,Input,Button,message, Form } from "antd";
+import FormItem from "antd/es/form/FormItem";
+
+const {TextArea} = Input;
 const CreateRestaurant = () => {
-    return (
-        <div> Create Restaurant</div>
-    );
-};
+        const onFinish = ({name, address, image}) =>{
+            if(!name){
+                message.error('Name required');
+                return;
+            }
+            if(!address){
+                message.error('Address required');
+                return;
+            }
+            if(!image){
+                message.error('Image link required');
+                return;
+            }
+            message.success('Restaurant details created!')
+        };
+    return (        
+            <Card title={'Restarurant Details'} style= {styles.page}>
+                <Form layout="vertical" on onFinish={onFinish}>
+                    <Form.Item label = {'Name'} required name = 'name'>
+                        <Input placeholder="Enter Name"/>
+                    </Form.Item>
+                    <Form.Item label ={'Address'} required name = 'address'>
+                        <Input placeholder="Enter Address" />
+                    </Form.Item>
+                    <Form.Item label={'Image'} required name = 'image'>
+                        <Input placeholder="Enter Image Link" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">Submit</Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        );
+    };
+    const styles = {
+        page:{
+            margin: 20,
+        },
+    }
+    
 
 export default CreateRestaurant;
