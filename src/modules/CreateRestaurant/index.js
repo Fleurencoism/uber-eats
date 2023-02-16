a3e79a946bb0ac0643f8f7dca64291c2609b2db9
Marlon Fleurencois <marlonfleurencois@gmai.com>
2023-02-16 00:46:21 -0500

    February 14

--- src/modules/CreateRestaurant/index.js
+++ src/modules/CreateRestaurant/index.js
@@ -1,9 +1,28 @@
 import { Card,Input,Button,message, Form } from "antd";
 import FormItem from "antd/es/form/FormItem";
+import { useEffect, useState } from "react";
+import { DataStore } from "aws-amplify";
+import { Restaurant } from "../../models";
+import { useRestaurantContext } from "../../context/RestaurantContext";
+
 
 const {TextArea} = Input;
 const CreateRestaurant = () => {
-        const onFinish = ({name, address, image}) =>{
+    const [name, setName] = useState(' ');
+    const [address, setAddress] = useState(' ');
+    const [image, setImage] = useState(' ');
+ 
+    const{sub, setRestaurant, restaurant} = useRestaurantContext();
+
+    useEffect(() =>{
+        if(!restaurant){
+            return;
+        }
+        setName(restaurant.name);
+        setAddress(restaurant.address);
+        setImage(restaurant.image);
+    }, [restaurant]);
+    const onFinish = async() =>{
             if(!name){
                 message.error('Name required');
                 return;
@@ -16,19 +35,57 @@ const CreateRestaurant = () => {
                 message.error('Image link required');
                 return;
             }
-            message.success('Restaurant details created!')
+            if (!restaurant) {
+                await createNewRestaurant();
+            } else {
+                await updateRestaurant();
+            }
         };
+
+    const updateRestaurant = async () => {
+        const updateRestaurant = await DataStore.save(
+            Restaurant.copyOf(restaurant, (updated) => {
+                updated.name = name;
+                updated.address = address;
+                updated.image = image;
+            })
+        );
+        setRestaurant(updateRestaurant);
+        message.success('Restaurant updated!');
+    };
+    const createNewRestaurant = async () => {
+        const newRestaurant = DataStore.save(new Restaurant({
+            name,
+            image,
+            address,
+            adminSub: sub,
+        }));
+        setRestaurant(newRestaurant);
+        message.success('Restaurant created!');
+    };
     return (        
             <Card title={'Restarurant Details'} style= {styles.page}>
                 <Form layout="vertical" on onFinish={onFinish}>
-                    <Form.Item label = {'Name'} required name = 'name'>
-                        <Input placeholder="Enter Name"/>
+                    <Form.Item label = {'Name'} required >
+                        <Input 
+                            placeholder="Enter Name" 
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}                       
+                        />
                     </Form.Item>
-                    <Form.Item label ={'Address'} required name = 'address'>
-                        <Input placeholder="Enter Address" />
+                    <Form.Item label ={'Address'} required >
+                        <Input 
+                        placeholder="Enter Address" 
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        />
                     </Form.Item>
-                    <Form.Item label={'Image'} required name = 'image'>
-                        <Input placeholder="Enter Image Link" />
+                    <Form.Item label={'Image'} required >
+                        <Input 
+                        placeholder="Enter Image Link" 
+                        value={image}
+                        onChange={(e) => setImage(e.target.value)}
+                        />
                     </Form.Item>
                     <Form.Item>
                         <Button type="primary" htmlType="submit">Submit</Button>
